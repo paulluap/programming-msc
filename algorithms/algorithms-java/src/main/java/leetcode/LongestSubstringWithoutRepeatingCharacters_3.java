a83f8c109a35a5c14de176780047ca9874ff0d2a
paulluap <taotao.nov@gmail.com>
2019-11-20 00:20:03 +0800

    made logic clear

--- algorithms/algorithms-java/src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters_3.java
+++ algorithms/algorithms-java/src/main/java/leetcode/LongestSubstringWithoutRepeatingCharacters_3.java
@@ -5,12 +5,12 @@ import java.util.Map;
 
 public class LongestSubstringWithoutRepeatingCharacters_3 {
     public static void main(String[] args) {
-//        System.out.println(lengthOfLongestSubstring("abcabcbb")); //3
+        System.out.println(lengthOfLongestSubstring("abcabcbb")); //3
         System.out.println(lengthOfLongestSubstring("bbbbb")); //1
-//        System.out.println(lengthOfLongestSubstring("pwwkew")); //3
-//        System.out.println(lengthOfLongestSubstring("abcadj")); //5
-//        System.out.println(lengthOfLongestSubstring("cdd")); //2
-//        System.out.println(lengthOfLongestSubstring("abba")); //2
+        System.out.println(lengthOfLongestSubstring("pwwkew")); //3
+        System.out.println(lengthOfLongestSubstring("abcadj")); //5
+        System.out.println(lengthOfLongestSubstring("cdd")); //2
+        System.out.println(lengthOfLongestSubstring("abba")); //2
     }
 
     public static int lengthOfLongestSubstring(String s) {
@@ -18,9 +18,10 @@ public class LongestSubstringWithoutRepeatingCharacters_3 {
         //l is the latest accumulated length of the substring without repeating chars in the string interation
         //ll is the largest l possible
         int ll =0, l=0, chopOff=0;
-        for(int i=0; i<s.length(); i++){
+        for(int i=0; i<s.length(); i++){ //i is useless..
             char c = s.charAt(i);
-            if (indexMap.get(c) != null && indexMap.get(c) >= chopOff/*ignore what we have chopped off*/) {
+            System.out.println(i + ", last chopoff: " + chopOff + " current chopoff: " + indexMap.get(c));
+            if (indexMap.get(c) != null && (chopOff == 0 || indexMap.get(c) > chopOff)/*ignore what we have chopped off*/) {
                 //a,b,c,a -> (a),b,c,a : 3 - 0 = 3
                 chopOff = indexMap.get(c);
                 ll = Math.max(ll, l);
