6d53a84bac1d092544db47aa5acf11002b5a1d6d
paulluap <taotao.nov@gmail.com>
2019-11-28 15:23:32 +0800

    dfs pathto

--- algorithms/algorithms-java/src/main/java/graph/DepthFirstSearch.java
+++ algorithms/algorithms-java/src/main/java/graph/DepthFirstSearch.java
@@ -1,27 +1,43 @@
 package graph;
 
 import datastructure.Graph;
+import datastructure.Stack;
+import sun.tools.jstat.Literal;
 
 public class DepthFirstSearch {
 
-    private boolean marked[];
+    private boolean[] marked;
     private int n;
-
+    private int s;
+    private int[] edgeTo;
 
     //after the search, we need to answer queries : marked, count
     public DepthFirstSearch(Graph G, int s){
+        this.s = s;
         marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
         dfs(G, s);
-
     }
 
     private void dfs(Graph G, int s){
-        System.out.println("visit " + s);
         marked[s] = true;
         n++;
         for(int w: G.adj(s)){
-            if (!marked[w]) dfs(G, w);
+            if (!marked[w]) {
+                edgeTo[w] = s;
+                dfs(G, w);
+            }
         }
+    }
+
+    public Iterable<Integer> pathTo(int v){
+        Stack<Integer> path = new Stack<>();
+        if (!marked[v]) return path;
+        for(int x = v; x != s; x = edgeTo[x]){
+            path.push(x);
+        }
+        path.push(s);
+        return path;
     }
 
     //is v conneted to s?
@@ -32,6 +48,5 @@ public class DepthFirstSearch {
     public int count(){
         return n;
     }
-
 
 }
