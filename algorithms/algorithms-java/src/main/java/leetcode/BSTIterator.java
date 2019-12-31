16bbc3f5afefaefafdcf3f003d96c4a1b8d7bc86
paulluap <taotao.nov@gmail.com>
2019-12-31 22:35:57 +0800

    leetcode 30

---
+++ algorithms/algorithms-java/src/main/java/leetcode/BSTIterator.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+class BSTIterator {
+
+    public BSTIterator(TreeNode root) {
+        
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        
+    }
+}
---
+++ algorithms/algorithms-java/src/main/java/leetcode/SubstringWithConcatenationOfAllWords_30.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SubstringWithConcatenationOfAllWords_30 {
+
+    public static void main(String[] args) {
+        SubstringWithConcatenationOfAllWords_30 sol = new SubstringWithConcatenationOfAllWords_30();
+//        sol.findSubstring()
+    }
+
+    public List<Integer> findSubstring(String s, String[] words) {
+//        int L = words[0].length();
+//        Map<Character,Integer> headLetters = new HashMap<>();
+//        for(int i=0; i<words.length; i++){
+//            headLetters.put(words[i].charAt(0), i);
+//        }
+//
+//        for(int i=0;;){
+//            if (headLetters.containsKey(s.charAt(0))){
+//
+//            }
+//        }
+
+        return null;
+    }
+
+}
