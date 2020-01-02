735dda6df4c721b85995dd75965e299421156fa1
paulluap <taotao.nov@gmail.com>
2020-01-02 22:54:06 +0800

    leetcode 142

---
+++ algorithms/algorithms-java/src/main/java/leetcode/LinkedListCycle2_142.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class LinkedListCycle2_142 {
+    public ListNode detectCycle(ListNode head) {
+        HashSet visited = new HashSet();
+        for(ListNode x = head; x!=null; x = x.next){
+            if (visited.contains(x)){
+                return x;
+            }
+            visited.add(x);
+        }
+        return null;
+    }
+}
