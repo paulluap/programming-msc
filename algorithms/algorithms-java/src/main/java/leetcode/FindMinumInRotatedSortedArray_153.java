8890aa98a7b0a145362a7a767c1f57a7885d68ef
paulluap <taotao.nov@gmail.com>
2021-04-04 14:51:47 +0800

    update FindMinumInRotatedSortedArray_153

--- algorithms/algorithms-java/src/main/java/leetcode/FindMinumInRotatedSortedArray_153.java
+++ algorithms/algorithms-java/src/main/java/leetcode/FindMinumInRotatedSortedArray_153.java
@@ -2,38 +2,60 @@ package leetcode;
 
 public class FindMinumInRotatedSortedArray_153 {
     public static void main(String[] args){
-        System.out.println("hello world");
         FindMinumInRotatedSortedArray_153 solution = new FindMinumInRotatedSortedArray_153();
-        int[] input = new int[]{4, 5, 6, 7, 0, 1, 2};
-        int min = solution.findMin();
-        System.out.println("expected 0, actual : " + min);
+        System.out.println(solution.findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+        System.out.println(solution.findMin(new int[]{4, 5, 6, 7, 8, 0, 1}));
+        System.out.println(solution.findMin(new int[]{4, 5, 6, 7, 8, 9, 0}));
+        System.out.println(solution.findMin(new int[]{5, 6, 0, 1, 2, 3, 4}));
+        System.out.println(solution.findMin(new int[]{6, 0, 1, 2, 3, 4, 5}));
+        System.out.println(solution.findMin(new int[]{0, 1, 2, 3, 4, 5, 6}));
+        System.out.println(solution.findMin(new int[]{0,1}));
+
+
+        //[8,0,1]
+        //
     }
     /**
      * problem is find the inflection point
      * [4,5,6,7,0,1,2]
-     *
-     *
-     * 
      */
     public int findMin(int[] nums) {
         int lo=0;
         int hi=nums.length - 1;
-        while(lo<=hi){
-            int mid = lo + (hi - lo)/2;
-            //fully sorted, not rotated
-            if (nums[lo] <= nums[mid] && nums[mid] <= nums[hi]){
+
+        //now let's find the inflection point
+        while(lo <= hi){
+
+            if (lo == hi){
                 return nums[lo];
             }
-            //left not sorted, inflection point in the left
-            else if (nums[lo] > nums[mid]){
-                //search in the left
+
+            int mid = lo + (hi - lo) / 2;
+
+            //this comes first in case index is out of bound
+            if (nums[mid] > nums[mid+1]){
+                return nums[mid+1]; //the inflection point
+            }
+            if (mid > 0 && nums[mid-1] > nums[mid]){
+                return nums[mid]; //the inflection point
+            }
+
+            if (nums[mid] > nums[hi]) {
+                //inflection point on the right
+                lo = mid + 1;
+            }
+
+            else {
+                //inflection point on the left,  or actually sorted
                 hi = mid - 1;
             }
 
-            else if (nums[hi] < nums[mid]){
-                lo = mid + 1;
-            }
         }
-        throw new IllegalArgumentException("intput must be invalid ");
+
+        //end up here if the arraysy is already fully sorted (hi is always decreased until it hi and low crosses)
+        return nums[lo];
+
     }
+
+
 }
